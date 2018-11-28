32e0a46f2a3f213ba830102cf9cbc9bfd9b885cb
epistrephein <epistrephein@gmail.com>
2018-11-28 07:42:08 +0100

    Add status key spec

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -3,11 +3,13 @@
 RSpec.describe 'Rubies Web' do
   it 'loads the home page' do
     get '/'
+
     expect(last_response).to be_ok
   end
 
   it 'returns a 404 error' do
     get '/non-existent'
+
     expect(last_response.status).to be 404
   end
 end
