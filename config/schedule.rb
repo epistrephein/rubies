d58101eee99a747862b9a5d7243c1493e07ba1ef
Tommaso Barbato <epistrephein@gmail.com>
2020-04-16 10:43:32 +0200

    Rename whenever logs to rake.log

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,7 +2,7 @@
 
 set :bundle_command,       '/usr/local/bin/bundle exec'
 set :chronic_options,      hours24: true
-set :output,               error:   'log/stderr.log'
+set :output,               error:   'log/rake.log'
 
 set :environment_variable, 'REDIS_URL'
 set :environment,          'redis://localhost:6379/0'
