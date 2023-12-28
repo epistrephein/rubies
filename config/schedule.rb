368902a99109092be365babd21da9a4e86dadbdf
Tommaso Barbato <tommasobarbato@gmail.com>
2023-12-28 08:37:01 +0100

    Rename database variable in schedule for clarity

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-job_type :rake, 'cd :path && :database_variable=:database :bundle_command rake :task :output'
+job_type :rake, 'cd :path && :database_variable=:database_url :bundle_command rake :task :output'
 
 set :chronic_options,   hours24: true
 
 set :database_variable, 'REDIS_URL'
-set :database,          'redis://localhost:6379/0'
+set :database_url,      'redis://localhost:6379/0'
 
 every 1.hour do
   rake 'redis', output: 'log/redis.log'
